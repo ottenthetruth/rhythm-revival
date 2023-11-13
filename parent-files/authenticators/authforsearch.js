99a96e015780903c1dbac4e0ee16f2cccaededc2
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-11-13 11:41:54 +0100

    Add authforsearch.js

---
+++ parent-files/authenticators/authforsearch.js
@@ -0,0 +1,6 @@
+function authforsearch() {
+  const clientID = "e9fec6e1cb5241e0a41ab98db146bc3c";
+  const redirectURI = "https://ottenthetruth.github.io/rhythm-revival/searchforalbums/searchforalbums.html";
+  const spotifyAuthURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURI}&scope=user-library-read%20playlist-read-private`;
+  window.location.href = spotifyAuthURL;
+}
