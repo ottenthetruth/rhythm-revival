f76988c625dd8d101a699cab841b83a3880a51d0
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-11-14 22:07:21 +0100

    Add search with enter

--- searchforalbums/searchforalbums.js
+++ searchforalbums/searchforalbums.js
@@ -1,3 +1,4 @@
+/* display search results */
 async function getAlbums() {
     const accessToken = await getAccessToken();
 
@@ -36,3 +37,19 @@ async function getAlbums() {
         }
     }
 }
+
+/* search with enter */
+var searchbar1 = document.getElementById("mysearch");
+var searchbar2 = document.getElementById("mysearchartist");
+
+searchbar1.addEventListener("keypress", function(event) {
+  if(event.key === "Enter") {
+    document.getElementById("searchButton").click();
+  }
+});
+
+searchbar2.addEventListener("keypress", function(event) {
+  if(event.key === "Enter") {
+    document.getElementById("searchButton").click();
+  }
+});
