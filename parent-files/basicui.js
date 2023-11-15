2ff1b98568189c4367c1ef8cfc79c68da58287ff
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-11-15 21:33:21 +0100

    Create basicui.js

---
+++ parent-files/basicui.js
@@ -0,0 +1,63 @@
+   async function getUserProfile() {
+	   const accessToken = await getAccessToken("https://ottenthetruth.github.io/truthmusic/homepage/homepage.html");
+        const response = await fetch('https://api.spotify.com/v1/me', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            },
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            const myDisplayName = data.display_name;
+            const profilePictureUrl = data.images.length > 0 ? data.images[0].url : '';
+
+            var myUserImage = document.getElementById('profileImage');
+            myUserImage.src = profilePictureUrl;
+            var myUserDisplayName = document.getElementById('displayName');
+            myUserDisplayName.innerText = myDisplayName;
+	    var logButton = document.getElementById("logBtn");
+	    logButton.style.display = "none";
+	    logButton.disabled = true;
+		alert("successful");
+        } else {
+            alert("Not successful");
+        }
+    }
+    async function getCurrentlyPlaying() {
+        const accessToken = localStorage.getItem("access_token");
+
+        if (accessToken) {
+            // Fetch currently playing track
+            const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+            });
+
+            if (response.status === 200) {
+                const data = await response.json();
+                const albumName = data.item.album.name;
+                const artistName = data.item.artists[0].name;
+                const albumCoverUrl = data.item.album.images[0].url;
+
+                document.getElementById('musicPlayerAlbum').textContent = albumName;
+                document.getElementById('musicPlayerArtist').textContent = artistName;
+
+                const albumCoverElement = document.getElementById('musicPlayerCover');
+                albumCoverElement.src = albumCoverUrl;
+                albumCoverElement.alt = `${albumName} Album Cover`;
+            }
+        }
+    }
+
+async function loadpage() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const code = urlParams.get("code");
+	if(code !== null){
+		getUserProfile();
+		getCurrentlyPlaying();
+	}
+} /* end loadpage */
+window.onload = loadpage;
