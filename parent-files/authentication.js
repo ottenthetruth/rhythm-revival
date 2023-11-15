c4842eb6a2d2d3a57f8327ebb8b66651ee64d22c
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-11-15 09:07:35 +0100

    Add authentication.js

---
+++ parent-files/authentication.js
@@ -0,0 +1,62 @@
+async function authforhomepage() {
+  /* check if theres already a token */
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+  /* end check */
+  if(code == null) {
+  const clientID = "e9fec6e1cb5241e0a41ab98db146bc3c";
+  const redirectURI = "https://ottenthetruth.github.io/rhythm-revival/homepage/homepage.html";
+  const callbackURL = "https://ottenthetruth.github.io/rhythm-revival/searchforalbums/searchforalbums.html";
+  const spotifyAuthURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURI}&scope=user-library-read%20playlist-read-private&state=${callbackURL}`;
+  window.location.href = spotifyAuthURL;
+  }
+}
+
+async function authforsearch() {
+  /* check if theres arleady a token */
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+  /* end check */
+  const clientID = "e9fec6e1cb5241e0a41ab98db146bc3c";
+  const redirectURI = "https://ottenthetruth.github.io/rhythm-revival/searchforalbums/searchforalbums.html";
+  const spotifyAuthURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURI}&scope=user-library-read%20playlist-read-private`;
+  window.location.href = spotifyAuthURL;
+}
+
+async function getAccessToken(myredirecturi) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+        if (code) {
+            const tokenEndpoint = "https://accounts.spotify.com/api/token";
+            const client_secret = "2d5a82decbc240e4adadcbd86f342321"; // Replace with your actual client secret
+            const redirect_uri = myredirecturi; // Make sure this matches your Spotify App's redirect URI
+            const basicAuthHeader = btoa(`e9fec6e1cb5241e0a41ab98db146bc3c:${client_secret}`);
+
+            const data = {
+                grant_type: "authorization_code",
+                code: code,
+                redirect_uri: redirect_uri,
+            };
+
+            const response = await fetch(tokenEndpoint, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Basic ${basicAuthHeader}`,
+                    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                },
+                body: new URLSearchParams(data),
+            });
+
+            if (response.status === 200) {
+                const tokenData = await response.json();
+                const accessToken = tokenData.access_token;
+
+                localStorage.setItem("access_token", accessToken);
+
+                return tokenData.access_token;
+            } else {
+              return localStorage.getItem("access_token");
+            }
+        }/* end if(code) */
+
+} /* end getAccessToken */
