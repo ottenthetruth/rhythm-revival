f155a4496e9a896a5ec9a1d2f10c56d01ee38eea
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-11-15 09:31:18 +0100

    Update explorebutton.js

--- homepage/explorebutton.js
+++ homepage/explorebutton.js
@@ -1,18 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () { /* check scroll height upon loading the page */
     checkScroll();
-
-    window.addEventListener('scroll', () => {
-        checkScroll();
-    });
-
-    document.getElementById('explore-button').addEventListener('click', () => {
+    window.addEventListener('scroll', () => { /* check after scrolling */
+    checkScroll(); });
+    document.getElementById('explore-button').addEventListener('click', () => { /* scroll to second part of the page upon clicking the button */
         window.scrollTo({
-            top: 1.2 * window.innerHeight,
-            behavior: 'smooth',
-        });
+        top: 1.2 * window.innerHeight,
+        behavior: 'smooth', });
     });
 
-function checkScroll() {
+function checkScroll() { /* update visibility of "explore"-button */
     const exploreButton = document.getElementById('explore-button');
     const exploreText = document.getElementById('myexploretext');
     if (window.scrollY === 0) {
@@ -23,5 +19,5 @@ function checkScroll() {
             exploreButton.style.opacity = 0;
             exploreText.style.opacity = 0;
             exploreButton.style.pointerEvents = 'none'; }
-}
+    } /* end checkScroll */
 });
