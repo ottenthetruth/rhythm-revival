f2428abe33f4914492c28c98c5f3cd82b8f2110f
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-11-30 14:43:38 +0100

    Add skip forward functionality / Test run 1

--- parent-files/musicplayer.js
+++ parent-files/musicplayer.js
@@ -1,5 +1,6 @@
 const pauseButton = document.getElementById('pauseButton');
 const resumeButton = document.getElementById('resumeButton');
+const skipForwardButton = document.getElementById('skipForward');
 
 pauseButton.addEventListener('click', () => {
     pauseButton.style.display = 'none';
@@ -11,6 +12,10 @@ resumeButton.addEventListener('click', () => {
     pauseButton.style.display = 'block';
     resumeButton.style.display = 'none';
     resumePlayback();
+});
+
+skipForwardButton.addEventListener('click', () => {
+    skipForward();
 });
 
 function pausePlayback() {
@@ -56,3 +61,25 @@ function resumePlayback() {
   });
   } //end if accessToken
 } //end resumePlayback
+
+function skipForward() {
+    cosnt accessToken = localStorage.getItem("access_token");
+    if(accessToken) {
+        fetch('https://api.spotify.com/v1/me/player/next', {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        }) // end fetch
+        .then(response => {
+        if (response.ok) {
+          console.log('Skipped to next song');
+        } else {
+          console.error('Unable to skip to next song');
+        }
+      })
+      .catch(error => {
+        console.error('Error skipping song:', error);
+      });        
+    } // end if accessToken
+} //end skipForward
