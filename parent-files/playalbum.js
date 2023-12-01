d152dcfea4e2af6b73131c58fcd54bd7373fbe04
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-01 16:56:10 +0100

    Create playalbum.js

---
+++ parent-files/playalbum.js
@@ -0,0 +1,24 @@
+function playAlbum(contextUri) {
+  const accessToken = localStorage.getItem("access_token"); // Replace with your Spotify access token
+
+  fetch(`https://api.spotify.com/v1/me/player/play`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      context_uri: contextUri,
+    }),
+  })
+  .then(response => {
+    if (response.status === 204) {
+      console.log('Album is playing!');
+    } else {
+      console.error('Failed to play album:', response.statusText);
+    }
+  })
+  .catch(error => {
+    console.error('Error occurred:', error);
+  });
+}
