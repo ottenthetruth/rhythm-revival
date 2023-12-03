80500e76519c855992c881cea2a918be5e2c727a
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-03 19:24:14 +0100

    Add getAvailableDevices

--- parent-files/playalbum.js
+++ parent-files/playalbum.js
@@ -1,7 +1,8 @@
 function playAlbum(contextUri) {
   const accessToken = localStorage.getItem("access_token");
-
-  fetch(`https://api.spotify.com/v1/me/player/play`, {
+  const deviceID = localStorage.getItem("device_id");
+  if(accessToken && deviceID) {
+  fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
     method: 'PUT',
     headers: {
       'Authorization': `Bearer ${accessToken}`,
@@ -20,5 +21,35 @@ function playAlbum(contextUri) {
   })
   .catch(error => {
     console.error('Error occurred:', error);
+  });
+  } // end if accessToken & device ID
+}
+
+function getAvailableDevices() {
+  const accessToken = localStorage.getItem("access_token");
+
+  fetch('https://api.spotify.com/v1/me/player/devices', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+    },
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to fetch device IDs');
+    }
+  })
+  .then(data => {
+    const devices = data.devices;
+    const deviceIds = devices.map(device => device.id);
+    if(devices.length > 0) {
+      const deviceID = devices[0].id;
+      console.log('Obtained device IDs!');
+      localStorage.setItem("device_id", deviceID); }
+  })
+  .catch(error => {
+    console.error('Error occurred while fetching device IDs:', error);
   });
 }
