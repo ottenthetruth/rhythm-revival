46ee0ad64da2c4c5ba3e3f3f66cb3c2837b82d2c
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-03 15:17:10 +0100

    remove loadPage

--- parent-files/basicui.js
+++ parent-files/basicui.js
@@ -1,28 +1,4 @@
-   async function getUserProfile() {
-	const accessToken = await getAccessToken("https://ottenthetruth.github.io/truthmusic/homepage/homepage.html");
-        const response = await fetch('https://api.spotify.com/v1/me', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-            },
-        });
-
-        if (response.status === 200) {
-            const data = await response.json();
-            const myDisplayName = data.display_name;
-            const profilePictureUrl = data.images.length > 0 ? data.images[0].url : '';
-
-            var myUserImage = document.getElementById('profileImage');
-            myUserImage.src = profilePictureUrl;
-            var myUserDisplayName = document.getElementById('displayName');
-            myUserDisplayName.innerText = myDisplayName;
-	    var logButton = document.getElementById("logBtn");
-	    logButton.style.display = "none";
-	    logButton.disabled = true;
-        }
-    } //end getUserProfile
-
-async function loadpage() {
+async function getUserProfile() {
   const accessToken = localStorage.getItem("access_token");
   if(accessToken) {
         const response = await fetch('https://api.spotify.com/v1/me', {
@@ -54,4 +30,4 @@ async function loadpage() {
   }
 } /* end loadpage */
 
-window.onload = loadpage;
+window.onload = getUserProfile;
