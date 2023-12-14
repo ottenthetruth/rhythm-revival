482b458d2c41802bcc7d8fed4ba9b1cfe039f2e6
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-14 23:24:17 +0100

    Bugfix V1

--- myprofile/myprofile.js
+++ myprofile/myprofile.js
@@ -1,6 +1,6 @@
 async function getTopTracks() {
   const accessToken = localStorage.getItem('access_token');
-  const response = await fetch(`${endpoint}/tracks?time_range=short_term&limit=${limit}`, {
+  const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=${limit}`, {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
@@ -11,7 +11,7 @@ async function getTopTracks() {
 
 async function getTopArtists() {
   const accessToken = localStorage.getItem('access_token');
-  const response = await fetch(`${endpoint}/artists?time_range=short_term&limit=${limit}`, {
+  const response = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=${limit}`, {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
@@ -19,6 +19,3 @@ async function getTopArtists() {
   const data = await response.json();
   console.log('Top Artists:', data.items);
 }
-
-getTopTracks();
-getTopArtists();
