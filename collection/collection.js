d6682f6584edb01f81e3daaeef25120fd1c6bd4e
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-16 20:23:23 +0100

    Add rating to display

--- collection/collection.js
+++ collection/collection.js
@@ -3,33 +3,38 @@ let myCollectionString = localStorage.getItem("mycollection");
 if(myCollectionString) {
   let myCollection = JSON.parse(myCollectionString);
   let itemCount = myCollection.length / 4;
-  const albumContainer = document.getElementById("albumContainer");
   let spotifyRequestIDs = "ids=";
   for(let i = 0; i < itemCount; i++) {
       const albumIDIndex = (i * 4) + 2;
       spotifyRequestIDs += myCollection[albumIDIndex];
         if (i !== itemCount - 1) { spotifyRequestIDs += ","; }
   }
+  
   const accessToken = localStorage.getItem("access_token");
   const response = await fetch(`https://api.spotify.com/v1/albums?${spotifyRequestIDs}`, {
-  method: 'GET',
-  headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
   });
+  
   if (response.status === 200) {
     const data = await response.json();
     const albumContainer = document.getElementById("albumContainer");
     if(data) {
+      let myRatingIndex = 1;
       data.albums.forEach(album => {
-          const card = document.createElement("div");
-          card.classList.add("card");
-          card.innerHTML = `
+          const collectionCard = document.createElement("div");
+          let myRating = myCollection[myRatingIndex];
+          myRatingIndex = myRatingIndex + 4;
+          collectionCard.classList.add("card");
+          collectionCard.innerHTML = `
           <img src="${album.images[0].url}" style="width: 200px; height: 200px;">
           <h4>${album.name}</h4>
           <h5>${album.artists[0].name}</h5>
+          <h5>${myRating}</h5>
           `;
-          albumContainer.appendChild(card);
+          albumContainer.appendChild(collectionCard);
         });
     }
   }
