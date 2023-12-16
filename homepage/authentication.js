b466e7431b1b9c255094a94f9e975dbb80a9adfc
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-16 22:28:41 +0100

    Update getRefreshToken

--- homepage/authentication.js
+++ homepage/authentication.js
@@ -18,13 +18,11 @@ async function getAccessToken() {
             const client_secret = "2d5a82decbc240e4adadcbd86f342321";
             const redirect_uri = "https://ottenthetruth.github.io/truthmusic/homepage/homepage.html";
             const basicAuthHeader = btoa(`e9fec6e1cb5241e0a41ab98db146bc3c:${client_secret}`);
-
             const data = {
                 grant_type: "authorization_code",
                 code: code,
                 redirect_uri: redirect_uri,
             };
-
             const response = await fetch(tokenEndpoint, {
                 method: "POST",
                 headers: {
@@ -33,47 +31,38 @@ async function getAccessToken() {
                 },
                 body: new URLSearchParams(data),
             });
-
             if (response.status === 200) {
                 const tokenData = await response.json();
                 const accessToken = tokenData.access_token;
                 const refreshToken = tokenData.refresh_token;
-
                 localStorage.setItem("access_token", accessToken);
                 localStorage.setItem("refresh_token", refreshToken);
                 console.log("Successfully obtained access token!");
                 return accessToken;
-            } else {
-              console.log("Access Token Status Response = " + response.status);
             }
         }/* end if(code) */
-
 } /* end getAccessToken */
 
-const getRefreshToken = async () => {
+async function getRefreshToken() {
   const refreshToken = localStorage.getItem('refresh_token');
-  const clientId = 'e9fec6e1cb5241e0a41ab98db146bc3c';
-  const url = "https://accounts.spotify.com/api/token";
-
-  const payload = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token: refreshToken,
-      client_id: clientId
-    }),
-  };
-
-  try {
-    const body = await fetch(url, payload);
-    const response = await body.json();
-
-    localStorage.setItem('access_token', response.access_token);
-    localStorage.setItem('refresh_token', response.refresh_token);
-  } catch (error) {
-    console.error('Error refreshing token:', error);
-  }
-};
+  if(refreshToken) {
+    const basicAuthHeader = btoa(`e9fec6e1cb5241e0a41ab98db146bc3c:${client_secret}`);
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Basic ${basicAuth}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        'grant_type': 'refresh_token',
+        'refresh_token': refreshToken,
+      }),
+    });
+    if(response.status === 200) {
+      const data = await response.json();
+      const accessToken = data.access_token;
+      localStorage.setItem("access_token", accessToken);
+      console.log("Updated Access Token!");
+    }
+  } /*if refreshtoken */
+}
