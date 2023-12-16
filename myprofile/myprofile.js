33150a0ae8f4d4d0a18ac825121111fc66133fe0
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-16 12:09:30 +0100

    Update Artist, Track info

--- myprofile/myprofile.js
+++ myprofile/myprofile.js
@@ -1,6 +1,6 @@
 async function getTopTracks() {
   const accessToken = localStorage.getItem('access_token');
-  const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10`, {
+  const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5`, {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
@@ -9,20 +9,22 @@ async function getTopTracks() {
   data.items.forEach((track, index) => {
     const currentTrackElementText = document.getElementById(`topsongname${index}`);
     const currentTrackElementImage = document.getElementById(`topsongimage${index}`);
+    const currentTrackElementArtist = document.getElementById(`topsonginfo${index}`);
     if(currentTrackElementText) {
       currentTrackElementText.textContent = track.name;
     }
-    if(currentTrackElementImage) {
+    if(currentTrackElementArtist) {
+      currentTrackElementArtist.textContent = "by " + track.artists[0].name;
+    }
       currentTrackElementImage.src = track.album.images[0].url;
       currentTrackElementImage.alt = `Track ${index + 1} Cover`;
     }
-    console.log(`Track ${index + 1}: ${track.name}`);
   });
 }
 
 async function getTopArtists() {
   const accessToken = localStorage.getItem('access_token');
-  const response = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=10`, {
+  const response = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5`, {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
@@ -31,13 +33,16 @@ async function getTopArtists() {
   data.items.forEach((artist, index) => {
     const currentArtistElementText = document.getElementById(`topartistname${index}`);
     const currentArtistElementImage = document.getElementById(`topartistimage${index}`);
+    const currentArtistElementGenre = document.getElementById(`topartistinfo${index}`);
     if(currentArtistElementText) {
       currentArtistElementText.textContent = artist.name;
+    }
+    if(currentArtistElementGenre){
+      currentArtistElementGenre.textContent = artist.genres[0] + ", " + artist.genres[1];
     }
     if(currentArtistElementImage) { 
       currentArtistElementImage.src = artist.images[0].url;
       currentArtistElementImage.alt = `Artist ${index + 1} Profile Picture`;
     }
-    console.log(`Artist ${index + 1}: ${artist.name}`);
   });
 }
