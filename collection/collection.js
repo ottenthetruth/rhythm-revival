2ddf8a532ed782d3a98a20aa628fa9f55a19a5ca
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-12-18 19:21:22 +0100

    Add starratings to collectionCard

--- collection/collection.js
+++ collection/collection.js
@@ -35,6 +35,20 @@ if(myCollectionString) {
             <h1>${album.artists[0].name}'s ${album.name}</h1>
             <h2>${album.total_tracks} Tracks, Released in ${releaseDate}</h2>
             <h2>Rating: ${myRating}</h2>
+            <div class="ratedisplay">
+              <fieldset class="rate">
+                <input type="radio" id="rating10" name="rating" value="10" /><label for="rating10" title="5 stars"></label>
+                <input type="radio" id="rating9" name="rating" value="9" /><label class="half" for="rating9" title="4 1/2 stars"></label>
+                <input type="radio" id="rating8" name="rating" value="8" /><label for="rating8" title="4 stars"></label>
+                <input type="radio" id="rating7" name="rating" value="7" /><label class="half" for="rating7" title="3 1/2 stars"></label>
+                <input type="radio" id="rating6" name="rating" value="6" /><label for="rating6" title="3 stars"></label>
+                <input type="radio" id="rating5" name="rating" value="5" /><label class="half" for="rating5" title="2 1/2 stars"></label>
+                <input type="radio" id="rating4" name="rating" value="4" /><label for="rating4" title="2 stars"></label>
+                <input type="radio" id="rating3" name="rating" value="3" /><label class="half" for="rating3" title="1 1/2 stars"></label>
+                <input type="radio" id="rating2" name="rating" value="2" /><label for="rating2" title="1 star"></label>
+                <input type="radio" id="rating1" name="rating" value="1" /><label class="half" for="rating1" title="1/2 star"></label>
+              </fieldset>
+            </div>
             <div class="collectionCardControls">
               <button class="collectionCardButtonPlay" data-context-uri="${album.uri}">Play!</button>
               <button class="collectionCardButtonRemove" data-context-uri="${album.uri}">Remove</button>
