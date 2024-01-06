16860847ef402f9b91cd923a80c1ee7be7b7f08a
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2024-01-06 13:16:16 +0100

    Create uploadPDF function

--- musichub/musichub.js
+++ musichub/musichub.js
@@ -1,17 +1,35 @@
 function uploadPDF() {
-    const fileInput = document.getElementById('fileInput');
+    const fileInput = document.getElementById('fileinput');
     const files = fileInput.files;
+
     if (files.length > 0) {
         const file = files[0];
         const reader = new FileReader();
         reader.onload = function(event) {
-            const pdfData = event.target.result;
-            localStorage.setItem('uploadedPDF', pdfData);
-            displayPDF(pdfData);
+            const pdfData = JSON.stringify(event.target.result);
+            const pdfName = JSON.stringify(file.name);
+            let prevFiles = [];
+            let prevFileNames = [];
+            const prevFilesString = localStorage.getItem("myfiles");
+            const prevFileNamesString = localStorage.getItem("myfilenames");
+            if (prevFilesString && prevFileNamesString) {
+                prevFiles = JSON.parse(prevFilesString);
+                prevFileNames = JSON.parse(prevFileNamesString);
+            }
+            const existingFileIndex = prevFileNames.indexOf(pdfName);
+            if (existingFileIndex !== -1) {
+                prevFiles[existingFileIndex] = pdfData;
+            } else {
+                prevFiles.push(pdfData);
+                prevFileNames.push(pdfName);
+            }
+            localStorage.setItem("myfiles", JSON.stringify(prevFiles));
+            localStorage.setItem("myfilenames", JSON.stringify(prevFileNames));
         };
         reader.readAsDataURL(file);
     }
 }
+
 
 function displayPDF(pdfData) {
     const pdfList = document.getElementById('pdfList');
